f1ecd66e568fae259583be60f9af7e1e757f5d15
Antoinette-Cordis <133567517+Antoinette-Cordis@users.noreply.github.com>
2023-05-23 14:54:08 -0700

    updated JS and made some Corrections in HTML.

--- Gitworks/index.js
+++ Gitworks/index.js
@@ -1,1 +1,49 @@
-
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
+}
+function weatherTemp(response) {
+  console.log(response);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+function search(city) {
+  let apiKey = "559d8dfd4e0451eaa6c405eb9092a204";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(weatherTemp);
+}
+function SearchForm(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-input");
+  search(cityElement.value);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", SearchForm);
