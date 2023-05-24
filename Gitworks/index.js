90f6c3aecc9367447222b17a99cd4c3f4df78c2f
Antoinette-Cordis <133567517+Antoinette-Cordis@users.noreply.github.com>
2023-05-24 15:16:58 -0700

    Updated the icon element in JS.

--- Gitworks/index.js
+++ Gitworks/index.js
@@ -34,6 +34,11 @@ function weatherTemp(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
