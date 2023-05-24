264aa5564142900c38202b7f0656d7a94c72d3d4
Antoinette-Cordis <133567517+Antoinette-Cordis@users.noreply.github.com>
2023-05-24 14:39:08 -0700

    Added Celsius and Fahrenheit unit conversion links in JS and updated the HTML and CSS to suit the JS.

--- Gitworks/index.js
+++ Gitworks/index.js
@@ -24,6 +24,8 @@ function weatherTemp(response) {
   console.log(response);
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemperature = response.data.main.temp;
+  tempElement.innerHTML = Math.round(celsiusTemperature);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   let humidityElement = document.querySelector("#humidity");
@@ -35,6 +37,21 @@ function weatherTemp(response) {
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+function showFahrenheittemp(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahrenheitTemp);
+}
+function showCelsiustemp(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(celsiusTemperature);
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+}
 function search(city) {
   let apiKey = "559d8dfd4e0451eaa6c405eb9092a204";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics`;
@@ -45,5 +62,11 @@ function SearchForm(event) {
   let cityElement = document.querySelector("#city-input");
   search(cityElement.value);
 }
+let celsiusTemperature = null;
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", SearchForm);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheittemp);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiustemp);
+search("Nigeria");
